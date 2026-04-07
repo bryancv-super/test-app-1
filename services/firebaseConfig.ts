5aac55bea5e61bfcbec4c040dd5f90388e4f289f
bryancv-super <bryancv2108@gmail.com>
2026-04-07 16:00:00 -0400

    feat: Installed firebase and added configuration file

---
+++ services/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCc5zMtfgC36EKeMdjEq7t9fXCaYThvwfk",
+  authDomain: "test-bryan-1-cb2ac.firebaseapp.com",
+  projectId: "test-bryan-1-cb2ac",
+  storageBucket: "test-bryan-1-cb2ac.firebasestorage.app",
+  messagingSenderId: "918354967401",
+  appId: "1:918354967401:web:8568bab1826b8ec79c9bf8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
