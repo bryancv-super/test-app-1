1ba22946431f051505cedc164639837c54492822
bryancv-super <bryancv2108@gmail.com>
2026-04-14 11:13:58 -0400

    feat: Add anonymous authentication logic

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,24 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useEffect } from "react";
+import { View, Text } from "react-native";
+import { loginAnonimo, observarAuth } from "../firebase/auth";
 
 export default function Index() {
+
+  useEffect(() => {
+    const unsubscribe = observarAuth((user: { uid: any; }) => {
+      if (user) {
+        console.log("Ya está logueado:", user.uid);
+      } else {
+        loginAnonimo();
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text>App con Firebase</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,4 +1,17 @@
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged, signInAnonymously } from "firebase/auth";
 import { app } from "./firebaseConfig";
 
 export const auth = getAuth(app);
+
+export const loginAnonimo = async () => {
+  try {
+    const userCredential = await signInAnonymously(auth);
+    console.log("Usuario anónimo:", userCredential.user.uid);
+  } catch (error) {
+    console.error("Error en login anónimo:", error);
+  }
+};
+
+export const observarAuth = (callback: any) => {
+  return onAuthStateChanged(auth, callback);
+};
