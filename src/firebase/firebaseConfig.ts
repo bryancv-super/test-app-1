1ac5469bf697e5b08c23321147c6d4b85ebeaa8d
bryancv-super <bryancv2108@gmail.com>
2026-04-14 11:03:16 -0400

    feat: Added firebase auth and firestore configuration

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCc5zMtfgC36EKeMdjEq7t9fXCaYThvwfk",
   authDomain: "test-bryan-1-cb2ac.firebaseapp.com",
@@ -13,5 +9,4 @@ const firebaseConfig = {
   appId: "1:918354967401:web:8568bab1826b8ec79c9bf8"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
